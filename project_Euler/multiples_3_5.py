134326d6b62a0fcc88bb5b733a37ebc9b28ca276
⟨Maj Bijol⟩ <mb52888@student.uni-lj.si>
2025-04-23 07:48:07 +0200

    popravil project euler nalogo

--- project_Euler/multiples_3_5.py
+++ project_Euler/multiples_3_5.py
@@ -1,8 +1,12 @@
 def multiples_3_5(upper_limit):
     multiples_3 = list(range(0,upper_limit,3))
     multiples_5 = list(range(0,upper_limit,5))
+    multiples_both = list(dict.fromkeys(multiples_3 + multiples_5))
+    for n in multiples_both:
+        if multiples_both.count(n) > 1:
+            multiples_both.pop(n)
     
-    multiples_sum = sum(list(dict.fromkeys(multiples_3 + multiples_5)))
+    multiples_sum = sum(multiples_both)
     
     return multiples_sum
 
